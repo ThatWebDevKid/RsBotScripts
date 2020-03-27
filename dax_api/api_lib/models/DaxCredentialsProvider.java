4b91f08c6552222f92d5d883d06e0a152d540101
ThatWebDevKid <62740687+ThatWebDevKid@users.noreply.github.com>
2020-03-27 03:48:03 -0400

    Remove DAX from scripts

--- dax_api/api_lib/models/DaxCredentialsProvider.java
+++
@@ -1,7 +0,0 @@
-package scripts.dax_api.api_lib.models;
-
-public abstract class DaxCredentialsProvider {
-
-    public abstract DaxCredentials getDaxCredentials();
-
-}
--- dax_api/teleport_logic/Validatable.java
+++
@@ -1,6 +0,0 @@
-package scripts.dax_api.teleport_logic;
-
-
-public interface Validatable {
-    boolean canUse();
-}
