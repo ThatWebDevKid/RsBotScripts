fe68b5be60e09a6baef6359194a43759d48dfb0c
ThatWebDevKid <62740687+ThatWebDevKid@users.noreply.github.com>
2020-03-28 18:18:42 -0400

    Update description of TutTheIsland

--- TutTheIsland/TutTheIsland.java
+++ TutTheIsland/TutTheIsland.java
@@ -30,7 +30,9 @@ import java.util.Collections;
         category = "Tutorial Island",
         name = "TutTheIsland",
         gameMode = 1,
-        description = "Tutorial Island Bot",
+        description = "Tutorial Island Bot by ThatWebDevKid! If you like it, please leave a comment and let me know! Any problems or bugs you run into," +
+                " please leave a comment and tell me the problem. Always looking to improve my scripts :D This script is fully open sourced. You can " +
+                "view the source code at https://github.com/ThatWebDevKid/RsBotScripts",
         version = (1.0)
 )
 
