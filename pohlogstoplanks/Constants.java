b352095407b9a70ad0fbdfdf3af45ae8b1550b9c
ThatWebDevKid <62740687+ThatWebDevKid@users.noreply.github.com>
2020-04-24 00:25:51 -0400

    Remove

--- pohlogstoplanks/Constants.java
+++
@@ -1,9 +0,0 @@
-package scripts.pohlogstoplanks;
-
-import org.tribot.api2007.types.RSArea;
-import org.tribot.api2007.types.RSTile;
-
-public class Constants {
-    public static final RSArea LUMBRIDGE_AREA = new RSArea(new RSTile(3217, 3221, 0), new RSTile(3226, 3214, 0));
-    public static final RSArea CAMELOT_AREA = new RSArea(new RSTile(2761, 3481, 0), new RSTile(2755, 3475, 0));
-}
